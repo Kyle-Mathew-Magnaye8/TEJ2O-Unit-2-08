99abae51dcc0f4eaf24981f68a23ff201cfebd1a
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-03-06 13:11:18 -0500

    updated code

--- main.ts
+++ main.ts
@@ -1,8 +1,56 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Feb 2026
+ * This program does an RGB LED by using circuits+.
 */
 
-basic.showString('Hello, World!')
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// when Button A is pressed it will scroll the text and the RGB light will turn each colors in order
+input.onButtonPressed(Button.A, function () {
+  // red
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.showString("RED")
+  basic.pause(1000)
+
+  // green
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.showString("GREEN")
+  basic.pause(1000)
+
+  // blue
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("BLUE")
+  basic.pause(1000)
+
+  // magenta
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("MAGENTA")
+  basic.pause(1000)
+
+  // cyan
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("CYAN")
+  basic.pause(1000)
+
+  // white
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("WHITE")
+  basic.pause(1000)
+})
